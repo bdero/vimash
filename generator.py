17fcb0bdd66c25ca4e894636fe96df7688eef1df
Brandon DeRosier <bdero@mit.edu>
2015-06-29 12:54:29 -0400

    Add YouTube search and result caching

--- generator.py
+++ generator.py
@@ -14,11 +14,51 @@ Options:
   -c --config=<config>       Path to config file [default: ./config.yml].
   -a --cache=<cache>         Location to store search cache [default: ./cache].
 """
-import yaml
+import os
 import sys
+import yaml
 
 from docopt import docopt
 from googleapiclient.discovery import build
+
+
+def youtube_search(keywords, developer_key,
+                   max_results='200', cache='./cache.yml'):
+    def read_cache(location):
+        try:
+            with open(location) as f:
+                contents = yaml.load(f)
+        except IOError:
+            contents = None
+        return contents
+
+    def write_cache(location, contents):
+        cache_dir = os.path.dirname(location)
+        if not os.path.exists(cache_dir):
+            os.mkdir(cache_dir)
+        with open(location, 'w') as f:
+            f.write(yaml.dump(contents))
+
+    search_results = {}
+    for keyword in keywords:
+        cache_path = os.path.join(cache, '{}.yml'.format(keyword))
+        # Try reading from the cache
+        keyword_results = read_cache(cache_path)
+
+        if keyword_results is None:
+            # If there's no cache for this term, do the search
+            print keyword, ': fetching results from YouTube.'
+            youtube = build('youtube', 'v3', developerKey=developer_key)
+            keyword_results = youtube.search().list(
+                q=keyword,
+                type='video',
+                part='id',
+                maxResults=max_results
+            ).execute()
+            write_cache(cache_path, keyword_results)
+        else:
+            print keyword, ': fetched results from cache.'
+        search_results[keyword] = keyword_results
 
 
 if __name__ == '__main__':
@@ -40,4 +80,9 @@ if __name__ == '__main__':
         print('Unable to read configuration file; aborting.')
         sys.exit(1)
 
-    print config
+    query_results = youtube_search(
+        config['keywords'],
+        config['DEVELOPER_KEY'],
+        max_results=config['max_search'],
+        cache=config['cache']
+    )
